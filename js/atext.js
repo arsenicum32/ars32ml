f05d945c3735e66c74fa55bc88a411dfb23500d5
arsenicum32 <zombiehot@yandex.ru>
2016-05-06 12:41:47 +0300

    mini mobile

--- js/atext.js
+++ js/atext.js
@@ -1,7 +1,7 @@
 atext.g('about',function(data){
   console.log(data);
-  $('h1 .about').text(data.h1);
-  $('p .about').text(data.p);
+  $('.about1').text(data.h1);
+  $('.about2').text(data.p);
 });
 
 // atext.dthelp.gdata( atext.dtjson[0], function(data){
