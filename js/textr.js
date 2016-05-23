69e792fd1174aa90bf8d69471ccefdb0c7c26818
arsenicum32 <zombiehot@yandex.ru>
2016-05-24 00:38:59 +0300

    test

---
+++ js/textr.js
@@ -0,0 +1,24 @@
+function rtext(){
+  function change(text){
+    var artext = text.split('');
+    var sym = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~ \t\n\r\x0b\x0c'.split('');
+    for(var i in artext){
+      if (artext[i] != ' ')
+      artext[i] = sym[ Math.floor(Math.random()*sym.length)];
+    }
+    return artext.join('');
+  }
+  $('.rtext').each(function(){
+    !$(this).attr('scopestart')?$(this).attr('scopestart', $(this).html()):void(0);
+    var changer = change( $(this).attr('scopestart') );
+    $(this).html(changer);
+
+    setTimeout(function(){
+      $(this).html( $(this).attr('scopestart') );
+    }, 100);
+  })
+}
+
+setInterval(function(){
+  rtext();
+}, 1000)
